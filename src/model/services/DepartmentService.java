20be45c83078bf189021ebecbc58d31b3f895786
“danielmarianii” <danielmariani10@hotmail.com>
2022-10-03 22:37:43 -0300

    DepartmentForm

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -9,9 +9,17 @@ import model.entities.Department;
 public class DepartmentService {
 
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
-	public List<Department>  findall(){
+
+	public List<Department> findall() {
 		return dao.findAll();
-		
+	}
+
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		} 
+		else {
+			dao.update(obj);
+		}
 	}
 }
